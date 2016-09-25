145dd1d8645062fbb12545ef17516878f032758b
Rene Hexel <r.hexel@griffith.edu.au>
2016-09-25 11:23:38 +0200

    Add CoglPango structs

--- coglpango_bridging.h
+++ coglpango_bridging.h
@@ -1,11 +1,7 @@
-struct _CoglBitmap {};
-struct _CoglFramebuffer {};
-struct _CoglMaterial {};
-struct _CoglMaterialLayer {};
-struct _CoglOffscreen {};
-struct _CoglPrimitive {};
-struct _CoglTexture {};
+struct _CoglPangoFontMap {};
+struct _CoglPangoRenderer {};
+struct _CoglPangoRendererClass {};
 
 #define COGL_ENABLE_EXPERIMENTAL_API
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
-#include <cogl/cogl.h>
+#include <cogl-pango/cogl-pango.h>
